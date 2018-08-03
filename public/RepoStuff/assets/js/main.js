9695d394e171fc27fb5f68d2841851c8d76f6acf
Throwback74 <corey.slade@gmail.com>
2018-08-02 22:42:21 -0700

    update progress, create a contact page working, logout buttons need testing, about to incorporate the contact list page

--- public/RepoStuff/assets/js/main.js
+++ public/RepoStuff/assets/js/main.js
@@ -26,76 +26,76 @@
 			}, 100);
 		});
 
-	// Nav.
-		var $nav_a = $nav.find('a');
+	// // Nav.
+	// 	var $nav_a = $nav.find('a');
 
-		$nav_a
-			.addClass('scrolly')
-			.on('click', function(e) {
+	// 	$nav_a
+	// 		.addClass('scrolly')
+	// 		.on('click', function(e) {
 
-				var $this = $(this);
+	// 			var $this = $(this);
 
-				// External link? Bail.
-					if ($this.attr('href').charAt(0) != '#')
-						return;
+	// 			// External link? Bail.
+	// 				if ($this.attr('href').charAt(0) != '#')
+	// 					return;
 
-				// Prevent default.
-					e.preventDefault();
+	// 			// Prevent default.
+	// 				e.preventDefault();
 
-				// Deactivate all links.
-					$nav_a.removeClass('active');
+	// 			// Deactivate all links.
+	// 				$nav_a.removeClass('active');
 
-				// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-					$this
-						.addClass('active')
-						.addClass('active-locked');
+	// 			// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+	// 				$this
+	// 					.addClass('active')
+	// 					.addClass('active-locked');
 
-			})
-			.each(function() {
+	// 		})
+	// 		.each(function() {
 
-				var	$this = $(this),
-					id = $this.attr('href'),
-					$section = $(id);
+	// 			var	$this = $(this),
+	// 				id = $this.attr('href'),
+	// 				$section = $(id);
 
-				// No section for this link? Bail.
-					if ($section.length < 1)
-						return;
+	// 			// No section for this link? Bail.
+	// 				if ($section.length < 1)
+	// 					return;
 
-				// Scrollex.
-					$section.scrollex({
-						mode: 'middle',
-						top: '-10vh',
-						bottom: '-10vh',
-						initialize: function() {
+	// 			// Scrollex.
+	// 				$section.scrollex({
+	// 					mode: 'middle',
+	// 					top: '-10vh',
+	// 					bottom: '-10vh',
+	// 					initialize: function() {
 
-							// Deactivate section.
-								$section.addClass('inactive');
+	// 						// Deactivate section.
+	// 							$section.addClass('inactive');
 
-						},
-						enter: function() {
+	// 					},
+	// 					enter: function() {
 
-							// Activate section.
-								$section.removeClass('inactive');
+	// 						// Activate section.
+	// 							$section.removeClass('inactive');
 
-							// No locked links? Deactivate all links and activate this section's one.
-								if ($nav_a.filter('.active-locked').length == 0) {
+	// 						// No locked links? Deactivate all links and activate this section's one.
+	// 							if ($nav_a.filter('.active-locked').length == 0) {
 
-									$nav_a.removeClass('active');
-									$this.addClass('active');
+	// 								$nav_a.removeClass('active');
+	// 								$this.addClass('active');
 
-								}
+	// 							}
 
-							// Otherwise, if this section's link is the one that's locked, unlock it.
-								else if ($this.hasClass('active-locked'))
-									$this.removeClass('active-locked');
+	// 						// Otherwise, if this section's link is the one that's locked, unlock it.
+	// 							else if ($this.hasClass('active-locked'))
+	// 								$this.removeClass('active-locked');
 
-						}
-					});
+	// 					}
+	// 				});
 
-			});
+	// 		});
 
 	// Scrolly.
-		$('.scrolly').scrolly();
+		// $('.scrolly').scrolly();
 
 	// Header (narrower + mobile).
 
